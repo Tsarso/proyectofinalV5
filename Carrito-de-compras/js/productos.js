00d6c095ac8f800a362be7660ed9bc55498f940f
Tsarso <carlosbrandonmeridacarrasco@gmail.com>
2024-11-29 08:46:32 -0400

    nueva interfaz

---
+++ Carrito-de-compras/js/productos.js
@@ -0,0 +1,37 @@
+const productos = [
+  {
+    id:1,
+    nombre: "Servilletas sencillas",
+    precio: 30,
+  },
+  {
+    id:2,
+    nombre: "Servilletas premium",
+    precio: 50,
+  },
+  {
+    id:3,
+    nombre: "Papel higiénico Vanisoft",
+    precio: 40,
+  },
+  {
+    id:4,
+    nombre: "Papel higiénico Holamigo",
+    precio: 45,
+  },
+  {
+    id:5,
+    nombre: "Pañuelos Holamigo",
+    precio: 25,
+  },
+  {
+    id:6,
+    nombre: "Papel higiénico Bailu",
+    precio: 35,
+  },
+  {
+    id:7,
+    nombre: "Papel de cocina",
+    precio: 40,
+  }
+]
